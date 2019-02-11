88e10a5b86e0bc45ae3e740b5b95499f8c2ba781
Toan Nguyen <toanngkh@gmail.com>
2019-02-10 19:23:06 -0600

    submit

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/pgapp'));
 
-app.get('/*', function (req, res) {
+// app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/pgapp/index.html'));
-});
+//   res.sendFile(path.join(__dirname + '/dist/pgapp/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
