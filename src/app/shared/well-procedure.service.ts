c7b361ca753ccad9892d4050139f1b523e2c21a5
Toan Nguyen <toanngkh@gmail.com>
2019-02-12 10:10:01 -0600

    change api host to azure

--- src/app/shared/well-procedure.service.ts
+++ src/app/shared/well-procedure.service.ts
@@ -11,7 +11,7 @@ export class WellProcedureService {
   dirSvyData: string;
   wellInfoList = [] as WellProcedureInput[];
 
-  readonly rootURL = 'http://40.113.216.254/api';
+  readonly rootURL = 'https://proceduregenerator20190210045919.azurewebsites.net/api';
   //readonly rootURL = 'https://proceduregenerator20190210045919.azurewebsites.net/api';
   //readonly csvUploadURL = 'https://localhost:44354/api';
 
